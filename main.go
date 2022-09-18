ac4e2f44786afd272564e9114e60e0b5d0e19057
Ben Visness <bvisness@gmail.com>
2022-09-18 16:25:30 -0500

    Save profiles via API

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	_ "embed"
+	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"runtime"
 	"strconv"
 	"strings"
@@ -17,20 +21,129 @@ import (
 //go:embed public/index.html
 var index []byte
 
+var (
+	pprofitPath  = filepath.Join(must1(os.UserHomeDir()), ".pprofit")
+	profilesPath = filepath.Join(pprofitPath, "profiles")
+)
+
+type obj map[string]any
+type arr []any
+
+var client = http.Client{}
+
 func main() {
-	host, port, err := getHostAndPort(":")
-	if err != nil {
-		panic(err)
+	must(os.MkdirAll(pprofitPath, 0750))
+	must(os.MkdirAll(profilesPath, 0750))
+
+	hostport := ":"
+	if len(os.Args) > 1 {
+		hostport = os.Args[1]
 	}
+	host, port := must2(getHostAndPort(hostport))
 	addr := fmt.Sprintf("%s:%d", host, port)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.Handle("/", GET(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
 		w.Write(index)
-	})
+	}))
+	http.Handle("/profiles", GET(func(w http.ResponseWriter, r *http.Request) {
+		type Profile struct {
+			Name string `json:"name"`
+			Type string `json:"type"`
+		}
+
+		files := must1(ioutil.ReadDir(profilesPath))
+		profiles := []Profile{}
+		for _, f := range files {
+			profiles = append(profiles, Profile{
+				Name: f.Name(),
+				Type: strings.SplitN(f.Name(), "-", 2)[0],
+			})
+		}
+		writeJSON(w, obj{
+			"profiles": profiles,
+		})
+	}))
+	http.Handle("/save", POST(func(w http.ResponseWriter, r *http.Request) {
+		type SaveRequest struct {
+			Url  string `json:"url"`
+			Type string `json:"type"`
+		}
+
+		var req SaveRequest
+		err := readJSON(r, &req)
+		if err != nil {
+			writeError(w, err, "")
+			return
+		}
+
+		if req.Url == "" {
+			writeError(w, nil, "missing `url`")
+			return
+		}
+		if req.Type == "" {
+			writeError(w, nil, "missing `type`")
+			return
+		}
+
+		res, err := client.Get(req.Url)
+		if err != nil {
+			writeError(w, err, "failed to get profile")
+			return
+		}
+		defer res.Body.Close()
+
+		outname := fmt.Sprintf("%s-%d", req.Type, time.Now().Unix())
+		out := must1(os.Create(filepath.Join(profilesPath, outname)))
+		must1(io.Copy(out, res.Body))
+
+		writeJSON(w, obj{
+			"name": outname,
+		})
+	}))
 
 	fmt.Println("Listening on", addr)
 	go openBrowser(fmt.Sprintf("http://%s/", addr))
 	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func GET(f http.HandlerFunc) http.Handler {
+	return Methods([]string{http.MethodGet}, f)
+}
+
+func POST(f http.HandlerFunc) http.Handler {
+	return Methods([]string{http.MethodPost}, f)
+}
+
+func Methods(methods []string, f http.HandlerFunc) http.HandlerFunc {
+	return Recover(func(w http.ResponseWriter, r *http.Request) {
+		validMethod := false
+		for _, method := range methods {
+			if r.Method == method {
+				validMethod = true
+			}
+		}
+		if !validMethod {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		f(w, r)
+	})
+}
+
+func Recover(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("PANIC: %v", r)
+				writeError(w, nil, fmt.Sprintf("request panicked: %v", r))
+			}
+		}()
+		f(w, r)
+	}
 }
 
 // From pprof source
@@ -103,4 +216,78 @@ func browsers() []string {
 		}
 	}
 	return cmds
+}
+
+// Takes an (error) return and panics if there is an error.
+// Helps avoid `if err != nil` in scripts. Use sparingly in real code.
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Takes a (something, error) return and panics if there is an error.
+// Helps avoid `if err != nil` in scripts. Use sparingly in real code.
+func must1[T any](v T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+// Takes a (something, something, error) return and panics if there is an error.
+// Helps avoid `if err != nil` in scripts. Use sparingly in real code.
+func must2[T1, T2 any](v1 T1, v2 T2, err error) (T1, T2) {
+	if err != nil {
+		panic(err)
+	}
+	return v1, v2
+}
+
+type userError struct {
+	error
+}
+
+func readJSON(r *http.Request, dest any) error {
+	if r.Header.Get("Content-Type") != "application/json" {
+		return userError{fmt.Errorf("expected Content-Type: application/json, but got %s", r.Header.Get("Content-Type"))}
+	}
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	err = json.Unmarshal(bodyBytes, dest)
+	if err != nil {
+		return userError{err}
+	}
+
+	return nil
+}
+
+func writeJSON(w http.ResponseWriter, v any) {
+	w.Header().Add("Content-Type", "application/json")
+	must1(w.Write(must1(json.Marshal(v))))
+}
+
+func writeError(w http.ResponseWriter, err error, niceMessage string) {
+	code := http.StatusInternalServerError
+	if _, ok := err.(userError); ok {
+		code = http.StatusBadRequest
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	if err != nil {
+		log.Printf("ERROR: %v", err)
+	}
+
+	message := niceMessage
+	if niceMessage == "" && err != nil {
+		message = err.Error()
+	}
+	must1(w.Write(must1(json.Marshal(obj{
+		"error": message,
+	}))))
 }
